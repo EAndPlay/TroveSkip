c34ea75c48e556e7976687a0f8b7ac134976290f
Andrey Brown <andrew-gonec@mail.ru>
2022-06-12 21:30:27 +0300

    Removed extra references

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -15,8 +15,8 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Threading;
-using TroveSkip.Annotations;
 using TroveSkip.Models;
+using TroveSkip.Properties;
 
 namespace TroveSkip.ViewModels
 {
